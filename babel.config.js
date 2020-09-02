d1c8ecba69d95cb546752fbf75852e6075edfb7f
1 <a1@1s-MacBook-Pro.lan>
2020-09-02 19:15:13 +0300

    chore(app): configured Babel, metro-config, package.json, tsconfig

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          api: './src/api',
+          assets: './src/assets',
+          components: './src/components',
+          configs: './src/configs',
+          features: './src/features',
+          navigation: './src/navigation',
+          stores: './src/stores',
+          theme: './src/theme',
+          utils: './src/utils',
+        },
+      },
+    ],
+  ],
 };
